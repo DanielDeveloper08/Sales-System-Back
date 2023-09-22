bd898abe72e4f51da367561258706d67b98280df
DanielDeveloper08 <danielprogramando2000@gmail.com>
2023-09-22 07:42:15 -0500

    Initial back

---
+++ SistemaVenta.DAL/Repositorios/VentaRepository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaVenta.DAL.DBContext;
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVenta.Model;
+
+namespace SistemaVenta.DAL.Repositorios
+{
+    public class VentaRepository : GenericRepository<Venta>, IVentaRepository
+    {
+        private readonly DbventaContext _dbventaContext;
+        public VentaRepository(DbventaContext dbventaContext) : base(dbventaContext)
+        {
+            _dbventaContext = dbventaContext;
+        }
+
+        public  async Task<Venta> Registrar(Venta modelo)
+        {
+            Venta ventaGenerada = new Venta();
+
+            using ( var transaction = _dbventaContext.Database.BeginTransaction()) 
+            {
+                try
+                {
+                    foreach(var dv in modelo.DetalleVenta)
+                    {
+                        Producto producto_encontrado = _dbventaContext.Productos.Where(p => p.IdProducto == dv.IdProducto).First();
+                        producto_encontrado.Stock = producto_encontrado.Stock - dv.Cantidad;
+                        _dbventaContext.Productos.Update(producto_encontrado);
+                    }
+
+                    await _dbventaContext.SaveChangesAsync();
+
+                    NumeroDocumento correlativo = _dbventaContext.NumeroDocumentos.First();
+                    correlativo.UltimoNumero = correlativo.UltimoNumero + 1;
+                    correlativo.FechaRegistro = DateTime.Now;
+
+                    _dbventaContext.NumeroDocumentos.Update(correlativo);
+                    await _dbventaContext.SaveChangesAsync();
+
+                    int CantidadDigitos = 4;
+                    string ceros = string.Concat(Enumerable.Repeat("0", CantidadDigitos));
+                    //0000
+                    string numeroVenta = ceros + correlativo.UltimoNumero.ToString();
+                    //0001
+                    numeroVenta = numeroVenta.Substring(numeroVenta.Length - CantidadDigitos, CantidadDigitos);
+
+                    modelo.NumeroDocumento = numeroVenta;
+
+                    await _dbventaContext.Venta.AddAsync(modelo);
+                    await _dbventaContext.SaveChangesAsync();
+
+                    ventaGenerada = modelo;
+
+                    transaction.Commit();
+
+                     
+                }catch
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+
+            }
+
+            return ventaGenerada;
+
+        }
+    }
+}
---
+++ SistemaVenta.Utility/AutoMapperProfile.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AutoMapper;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemaVenta.DTO;
+using SistemaVenta.Model;
+
+namespace SistemaVenta.Utility
+{
+    public class AutoMapperProfile: Profile
+    {
+        public AutoMapperProfile()
+        {
+            #region Rol
+            CreateMap<Rol,RolDTO>().ReverseMap();
+            #endregion Rol
+
+            #region Menu
+            CreateMap<Menu, MenuDTO>().ReverseMap();
+            #endregion Menu
+
+            #region Usuario
+            CreateMap<Usuario, UsuarioDTO>()
+                .ForMember(destino =>
+                    destino.RolDescripcion,
+                    opt => opt.MapFrom(origen => origen.IdRolNavigation.Nombre)
+                 )
+                .ForMember(destino =>
+                    destino.EsActivo,
+                    otp => otp.MapFrom(origen => origen.EsActivo == true ? 1 : 0)
+                 );
+
+            CreateMap<Usuario, SesionDTO>()
+                .ForMember(destino =>
+                    destino.RolDescripcion,
+                    opt => opt.MapFrom(origen => origen.IdRolNavigation.Nombre)
+                 );
+
+            CreateMap<UsuarioDTO, Usuario>()
+                .ForMember(destino =>
+                    destino.IdRolNavigation,
+                    opt => opt.Ignore())
+                .ForMember(destino =>
+                    destino.EsActivo,
+                    otp => otp.MapFrom(origen => origen.EsActivo == 1 ? true : false)
+                 );
+
+            #endregion Usuario
+
+            #region Categoria
+            CreateMap<Categoria, CategoriaDTO>().ReverseMap();
+            #endregion Rol
+
+            #region Producto
+            CreateMap<Producto, ProductoDTO>()
+                .ForMember(destino =>
+                    destino.DescripcionCategoria,
+                    opt => opt.MapFrom(origen => origen.IdCategoriaNavigation.Nombre))
+                .ForMember(destino =>
+                    destino.Precio,
+                    otp => otp.MapFrom(origen => Convert.ToString(origen.Precio.Value, new CultureInfo("es-ECU"))))
+                .ForMember(destino =>
+                    destino.EsActivo,
+                    otp => otp.MapFrom(origen => origen.EsActivo == true ? 1 : 0)
+                 );
+            #endregion Producto
+        }
+    }
+}
